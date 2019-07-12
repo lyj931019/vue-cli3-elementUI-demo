76c5606e5de91b5789ce42033da83e1dde82272c
lyj931019 <2232572692@qq.com>
2019-07-12 22:00:37 +0800

    简单配置router

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -19,7 +19,37 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      meta:{
+        isLogin : true, // 自定义一个属性，true需要登录权限
+        title:"主页"
+      },
     }
   ]
 })
+
+//导航前守卫(跳转前)
+router.beforeEach(function(to,from,next){
+    // to: 表示要跳转的页面。from:表示从哪个页面来
+    let login_in = localStorage.login                  // 是否已登录
+    let require = to.matched.some(function(item){      // 是否需要登录
+        return item.meta.isLogin
+    })
+    if( !login_in && require ){       // 当未登录，且跳转的页面需要登录后才能操作时，进行路由拦截
+        next({                          // 跳转登录页
+            name: "login",
+            params: { redirect: to }      // 将 要跳转（即被拦截） 的路由对象，作为参数，传递到登录页面
+        });
+    }else{                            // 已登录就正常跳转，不做任何拦截
+        next()                          // 正常跳转到下一页
+    }
+})
+//导航后守卫(跳转后)
+router.afterEach(function(to,from){
+    document.title = to.meta.title     //跳转后设置页面的title
+})
+
+
+
+
+export default router
---
+++ src/views/login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+</template>
+
+<script>
+    // @ is an alias to /src
+    import HelloWorld from '@/components/HelloWorld.vue'
+
+    export default {
+        name: 'home',
+        components: {
+            HelloWorld
+        },
+        methodes:{
+            login(){
+//                localStorage.login = true // 登录状态
+                // this.$route.params.redirect 是路由拦截是传过来的拦截页面的路由对象，看下文的路由拦截配置详情，有说明。
+                if( this.$route.params.redirect ){   // 判断是否有redirect参数
+                    this.$router.push( this.$route.params.redirect ) //登录后跳回被的拦截
+                }
+            }
+        }
+    }
+</script>
